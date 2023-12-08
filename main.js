c5cd8b486fce068c9df85eae3a662da4f4ad44e3
Coty Sanchez <constanzasanchezdigital@gmail.com>
2023-12-08 12:48:07 -0300

    testing

--- main.js
+++ main.js
@@ -1,67 +1,191 @@
+const fs =require("fs").promises;
+
 class ProductManager {
 
     static ultId=0;
 
     constructor(){
         this.products= [];
+        this.path = path;
 
     }
     
-    addProduct(title, description, price, thumbnail, code, stock){
+     async addProduct (nuevoObjeto){
+
+        let {title, description, price, thumbnail, code, stock} = nuevoObjeto;
         
         if(!title || !description || !price || !thumbnail || !code || !stock){
             console.log (" Todos los campos son Obligatorios");
             return; 
         }
 
-    if (this.products.some ( item =>item.code === code)){
-        console.log("El codigo debe ser unico");
-        return;
+        if (this.products.some ( item =>item.code === code)){
+            console.log("El codigo debe ser unico");
+            return;
+        }
+
+        const newProduct = {
+            id: ++ProductManager.ultId,
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock
+        }
+
+        this.products.push(newProduct);
+
+        await this.guardarArchivo(this.products);
+
+
     }
 
-    const newProduct = {
-        id: ++ProductManager.ultId,
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock
+
+    getProducts() {
+        console.log(this.products);
     }
 
-    this.products.push(newProduct);
 
-}
+    async getProductById(id){
+        try{ 
+            const arrayProductos= await this.leerArchivo();
+            const buscado = arrayProductos.find(item => item.id === id);
 
-getProducts() {
-    console.log(this.products);
-}
+        if(!buscado){
+            console.log("Producto no encontrado");
+        }else{
+            console.log("Producto encontrado");
+            return buscado;
+        }
 
-getProductById(id){
-    const product= this.products.find (item=> item.id === id);
-
-    if(!product){
-        console.log("Producto no encontrado");
-    }else{
-        console.log("Producto encontrado:" , product);
+    }catch (error){
+        console.log ("Error al leer el archivo", error);
     }
-    return product;
+
+}
+    //Nuevos metodos desafio 2
+
+    async leeArchivo(){ 
+
+        try{
+            const respuesta= await fs.readFile(this.path, "utf-8");
+            const arrayProductos= JSON.parse(respuesta);
+            return arrayProductos;
+        
+
+
+        } catch (error){
+            console.log('Error al leer un Archivo', error);
+        }
+
+    }
+
+    async guardarArchivo(arrayProductos){
+
+        try {
+            await fs.writeFile(this.path, JSON.stringify(arrayProductos, null,2));
+        } catch (eror){
+            console.log("Error al Guardar el Archivo", error);
+        }
+
+    }
+
+    async updateProduct(id, productoActualizado){
+        try{
+            const arrayProductos = await this.leerArchivo();
+            const index = arrayProductos.findIndex(item=> item.id ===id);
+
+            if (index !== -1) {
+
+                arrayProductos.splice(index,1, productoActualizado);
+                await this.guardarArchivo(arrayProductos);
+            }else {
+                console.log("No se encontro el producto");
+            }
+
+    
+        }catch (error){
+            console.log ("Error al Actualizar el Producto", error);
+        }
+    }
 
 }
 
-}
 
 
-const manager = new ProductManager();
+
+
+//Testing 
+
+const manager = new ProductManager("./productos.json");
+
 manager.getProducts();
 
-manager.addProduct("producto prueba","este es un producto prueba", 200,"sin imagen","abc123",25);
-manager.addProduct("Reloj Tommy Hilfiger", "Reloj analogico", 1200, "sin imagen", "abc124", 50);
-manager.addProduct("Cadena Oro", "Cadena de Oro 18 k", 1400, "sin imagen", "abc125", 50);
+const reloj ={
+    title: "Tommy Hilfiger",
+    description: "Reloj Analogico Silver",
+    price: 800,
+    thumbnail:"sin imagen",
+    code: "abc123",
+    stock: 10
+
+}
+
+manager.addProduct(reloj);
+
+const cadena={
+    title:"Cadena Oro",
+    description:"Cadena Oro 18k",
+    price: 700,
+    thumbnail:"sin imagen",
+    code:"abc124",
+    stock: 20
+}
+
+manager.addProduct(cadena);
+
+const gafas = {
+  title: "Aviator RayBan",
+  description: "Modelo Aviator P RayBan",
+  price: 400,
+  thumbnail: 'sin imagen',
+  code: 'abc125',
+  stock: 30
+}
+
+manager.addProduct(gafas);
 
 manager.getProducts();
 
-manager.addProduct('Cadena Oro','Cadena de Oro 18 k',1400,'sin imagen','abc125',50);
+async function testeamosBusquedaPorId(){
+    const buscado=await manager.getProductById(2);
+    console.log(buscado);
+}
 
-manager.getProductById(2);
-manager.getProductById(50);
+testeamosBusquedaPorId();
+
+const pulsera ={
+    id:1,
+    title: "Pulsera",
+    description:"Pulsera oro 18k",
+    price: 400,
+    thumbnail:"sin imagen",
+    code: "abc123",
+    stock: 10
+};
+
+
+async function testeamosActualizar(){
+    await manager.updateProduct(1,pulsera);
+}
+
+testeamosActualizar();
+
+
+
+
+
+
+
+
